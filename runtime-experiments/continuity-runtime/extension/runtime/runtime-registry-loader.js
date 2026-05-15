d58181e90c92a9769de252900a6b5a88425af454
hmfbr-com <jean.paldanius@gmail.com>
2026-05-15 07:48:41 +0200

    Add extension registry loader

---
+++ runtime-experiments/continuity-runtime/extension/runtime/runtime-registry-loader.js
@@ -0,0 +1,14 @@
+window.continuityRuntimeRegistry = {
+  sources: [
+    'outsetdesign.org',
+    'fairdealpolicy.org',
+    'plab.studio'
+  ],
+  capabilities: [
+    'overlay-runtime',
+    'context-runtime',
+    'registry-loading'
+  ]
+};
+
+console.log('runtime_registry_loaded', window.continuityRuntimeRegistry);
